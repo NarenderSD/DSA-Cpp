8c915eeeb801b26e3b29d368c4ce2062380eb8b4
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-09-28 13:09:06 +0530

    DSA C++ typecasting

---
+++ Lecture 2/input.cpp
@@ -0,0 +1,19 @@
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Your age is: " << age << endl;
+
+    char grade;
+    cout << "Enter your grade: ";
+    cin >> grade;
+    cout << "Your grade is: " << grade << endl;
+    
+    return 0;   
+}
---
+++ Lecture 2/typecasting.cpp
@@ -0,0 +1,31 @@
+//*********************************************  Type Casting  *********************************************
+
+//--------------------------------------------   Canversion    --------------------------------------------
+
+// ++++++++++++++++++++++++++++++++++_____________ ye phle s fix hota hai ____________+++++++++++++++++++++++++++++++++
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     char grade = 'A'; //ASCII value of A is 65
+
+//     int value = grade;
+//     cout << value << endl;
+//     return 0;   
+// }
+
+
+//--------------------------------------------   Casting    --------------------------------------------
+// +++++++++++++++++++++++++++++++_____________ ese developere karta hai ____________++++++++++++++++++++++++++++
+#include <iostream>
+using namespace std;
+
+int main() {
+    double price = 100.50;
+
+    int newPrice = (int)price;
+    cout << newPrice << endl;
+    return 0;   
+}
