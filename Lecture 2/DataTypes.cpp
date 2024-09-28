26c809f24b94d45814a61566ff67efea9ce4c93a
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-09-28 12:55:44 +0530

    DSA C++ DataType

---
+++ Lecture 2/DataTypes.cpp
@@ -0,0 +1,47 @@
+// Data Types
+
+// int
+// 4 Bytes
+
+
+
+// char
+// 1 Byte
+
+
+// float
+// 4 Bytes
+
+// bool
+// 1 Byte
+// treu or false
+
+
+// double
+// 8 Bytes
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int age = 23;
+    char grade = 'A';
+    float PI = 3.14f;
+
+    bool isSafe = true;
+
+    double largePI = 3.14159265358979323846;
+    // cout << age  <<endl;
+    // cout << sizeof(age)  <<endl;
+
+    // cout << grade  <<endl;
+    // cout << PI  <<endl;
+
+    // cout << isSafe  <<endl; //true = 1, false = 0
+
+    cout << largePI  <<endl;
+    return 0;   
+}
