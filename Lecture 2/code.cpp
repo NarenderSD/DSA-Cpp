61314f2261ca90ea4d6359288936a982efbc3d42
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-09-28 12:17:22 +0530

    DSA C++ code

---
+++ Lecture 2/code.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<"Radhe Radhe\nRadha Krishna\n";
+    return 0;   
+}
+
+
+
+// --------------------------------------------   Boilerplate Code    --------------------------------------------
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     // Write your code here
+//     return 0;   
+// }
